c52f5bc747d2955669750952ade40728d425c9f7
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2018-06-04 03:31:41 +0530

    Done

--- Gui Interface.py
+++ Gui Interface.py
@@ -3,18 +3,29 @@ from tkinter import *
 
 
 root=Tk()
-topframe=Frame(root)
-topframe.pack(side=TOP)
-bottomframe=Frame(root)
-bottomframe.pack(side=BOTTOM)
+root.title("Login APP")
+root.geometry("400x400")
 
-button1= Button (topframe, Text="Login", fg="blue")
-button2= Button (topframe, Text="Password", fg="blue")
-button3= Button (bottomframe, Text="Submit", fg="red")
+label1=Label(text="Enter details")
+label1.grid(column=0,row=0)
 
-button1.pack()
-button2.pack()
-button3.pack()
+button1=Label(text="Username")
+button1.grid(column=20,row=1)
+entry1=Entry()
+entry1.grid(column=21,row=1)
+
+button2=Label(text="Password")
+button2.grid(column=20,row=2)
+entry2=Entry()
+entry2.grid(column=21,row=2)
+
+button3=Button(text="Login")
+button3.grid(column=20,row=3)
+
+
+
+
+
 
 
 
