11f438772eddb0863bc93d7c9e8ab839375177aa
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2018-06-04 01:06:14 +0530

    Backend of twiiter login

---
+++ Login.py
@@ -0,0 +1,30 @@
+from selenium import webdriver 
+from time import sleep
+from getpass import getpass
+
+usr=input("Enter Email Id:")
+pwd=getpass("Enter Password")
+
+driver=webdriver.Chrome()
+driver.get("https://twitter.com/login?lang=en")
+print("Opened Twitter")
+sleep(1)
+
+username=driver.find_element_by_id('email')
+username.send_keys(usr)
+print("Username entered")
+sleep(1)
+
+password=driver.find_element_by_id('password')
+password.send_keys(pwd) 
+print("Password entered")
+sleep(1)
+
+login_click=driver.find_element_by_id("loginbutton")
+login_click.click()
+
+
+print("All Done")
+input("if sucessfull input anything")
+driver.quit()
+print("Finished")
