4a4ae5e15d45e134cb15269f23bd00afc41b3720
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2018-06-04 04:56:26 +0530

    Final

--- Gui Interface.py
+++ Gui Interface.py
@@ -1,32 +1,26 @@
+import tkinter as tkk
 
-from tkinter import *
-
-
-root=Tk()
+    
+root=tkk.Tk()
 root.title("Login APP")
 root.geometry("400x400")
 
-label1=Label(text="Enter details",fg="Black",font=("Open Sans",10))
+label1=tkk.Label(text="Enter details",fg="Black",font=("Open Sans",10))
 label1.grid(column=0,row=0)
 
-button1=Label(text="Username",bg="black",fg="white",font=("Open Sans",10))
+button1=tkk.Label(text="Username",bg="black",fg="white",font=("Open Sans",10))
 button1.grid(column=20,row=1)
-entry1=Entry()
+
+entry1=tkk.Entry()
 entry1.grid(column=21,row=1)
 
-button2=Label(text="Password",bg="black",fg="white",font=("Open Sans",10))
+button2=tkk.Label(text="Password",bg="black",fg="white",font=("Open Sans",10))
 button2.grid(column=20,row=2)
-entry2=Entry()
+
+entry2=tkk.Entry()
 entry2.grid(column=21,row=2)
 
-button3=Button(text="Login")
+button3=tkk.Button(text="Login")
 button3.grid(column=20,row=3)
-
-
-
-
-
-
-
 
 root.mainloop()
--- Login.py
+++ Login.py
@@ -2,29 +2,63 @@ from selenium import webdriver
 from time import sleep
 from getpass import getpass
 import os
-usr=input("Enter Email Id:")
-pwd=getpass("Enter Password")
 
-driver=webdriver.Chrome("C:/Users/Utkarsh/Downloads/chromedriver_win32/chromedriver.exe")
-driver.get("https://www.facebook.com/")
-print("Opened Facebook")
-sleep(1)
+import tkinter as tkk
 
-username=driver.find_element_by_id('email')
-username.send_keys(usr)
-print("Username entered")
-sleep(1)
+usr1="as"
+pwd1="s"    
 
-password=driver.find_element_by_id('pass')
-password.send_keys(pwd) 
-print("Password entered")
-sleep(1)
+def start():
+ usr=usr1
+ pwd=pwd1
 
-login_click=driver.find_element_by_id("loginbutton")
-login_click.click()
+ driver=webdriver.Chrome("C:/Users/Utkarsh/Downloads/chromedriver_win32/chromedriver.exe")
+ driver.get("https://www.facebook.com/")
+ print("Opened Facebook")
+ sleep(1)
+
+ username=driver.find_element_by_id('email')
+ username.send_keys(usr)
+ print("Username entered")
+ sleep(1)
+
+ password=driver.find_element_by_id('pass')
+ password.send_keys(pwd) 
+ print("Password entered")
+ sleep(1)
+
+ login_click=driver.find_element_by_id("loginbutton")
+ login_click.click()
 
 
-print("All Done")
-input("if sucessfull input anything")
-driver.quit()
-print("Finished")
+ print("All Done")
+ input("if sucessfull input anything")
+ driver.quit()
+ print("Finished")
+ 
+
+
+root=tkk.Tk()
+root.title("Login APP")
+root.geometry("400x400")
+
+label1=tkk.Label(text="Enter details",fg="Black",font=("Open Sans",10))
+label1.grid(column=0,row=0)
+
+button1=tkk.Label(text="Username",bg="black",fg="white",font=("Open Sans",10))
+button1.grid(column=20,row=1)
+
+entry1=tkk.Entry(textvariable=usr1)
+entry1.grid(column=21,row=1)
+
+button2=tkk.Label(text="Password",bg="black",fg="white",font=("Open Sans",10))
+button2.grid(column=20,row=2)
+
+entry2=tkk.Entry(show="*",textvariable=pwd1)
+entry2.grid(column=21,row=2)
+
+button3=tkk.Button(text="Login",command=start)
+button3.grid(column=20,row=3)
+root.mainloop()
+
+
