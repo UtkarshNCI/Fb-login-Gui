99aaa2f20869a137c0a4aa78f2207d9a2534239c
RandomQWE <31191176+RandomQWE@users.noreply.github.com>
2018-06-05 03:05:03 +0530

    Final Version of gui login (Facebook)

--- Gui Interface.py
+++ Gui Interface.py
@@ -1,6 +1,46 @@
 import tkinter as tkk
+from selenium import webdriver 
+from time import sleep
+from getpass import getpass
+import os
 
-    
+text1=str       
+text2=str
+
+
+def clicked(*args):
+    text1=entry1.get()
+    text2=entry2.get()
+    cli(text1,text2)
+
+
+def cli(usr,pwd):
+    driver=webdriver.Chrome("C:/Users/Utkarsh/Downloads/chromedriver_win32/chromedriver.exe")
+    driver.get("https://www.facebook.com/")
+    print("Opened Facebook")
+    sleep(1)
+
+    username=driver.find_element_by_id('email')
+    username.send_keys(usr)
+    print("Username entered")
+    sleep(1)
+
+    password=driver.find_element_by_id('pass')
+    password.send_keys(pwd) 
+    print("Password entered")
+    sleep(1)
+
+    login_click=driver.find_element_by_id("loginbutton")
+    login_click.click()
+
+
+    print("All Done")
+    input("if sucessfull input anything")
+    driver.quit()
+    print("Finished")        
+
+
+
 root=tkk.Tk()
 root.title("Login APP")
 root.geometry("400x400")
@@ -17,10 +57,10 @@ entry1.grid(column=21,row=1)
 button2=tkk.Label(text="Password",bg="black",fg="white",font=("Open Sans",10))
 button2.grid(column=20,row=2)
 
-entry2=tkk.Entry()
+entry2=tkk.Entry(show="*")
 entry2.grid(column=21,row=2)
 
-button3=tkk.Button(text="Login")
+button3=tkk.Button(text="Login",command=clicked)
 button3.grid(column=20,row=3)
-
+root.bind("<Return>", clicked)
 root.mainloop()
